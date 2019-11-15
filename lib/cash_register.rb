36e9de37482257cc4391b834c7ff4f65d16abe52
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-15 17:41:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
-    @last_transaction == price * quantity
+    @last_transaction = price * quantity
     #except last_transaction instead of total, and not adding to it, but setting it equal to
     if quantity > 1
       counter = 0
